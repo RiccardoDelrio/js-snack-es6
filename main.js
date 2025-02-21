b853948a95e870a41289ae597f021ddc2c6666d9
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-02-21 16:13:06 +0100

    add card, form and add submit event for find most weight

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 /* Snack 1
 Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
 Stampare a schermo la bici con peso minore. */
+const form = document.querySelector("#formRequest")
+const text = document.querySelectorAll(".card-text")
 const negozio = [
     {
-        nome: "terminator",
+        nome: "Terminator",
         peso: 9,
     },
     {
@@ -26,14 +28,25 @@ const negozio = [
 ]
 let piuPesante = 100
 let bicicleta = {}
-for (i = 0; i < negozio.length; i++) {
-    if (negozio[i].peso < piuPesante) {
-        piuPesante = negozio[i].peso
-        bicicleta = negozio[i]
+
+
+
+
+form.addEventListener("submit", function (e) {
+    e.preventDefault()
+    for (i = 0; i < negozio.length; i++) {
+        if (negozio[i].peso < piuPesante) {
+            piuPesante = negozio[i].peso
+            bicicleta = negozio[i]
+        }
+
     }
 
-}
-console.log(piuPesante);
-console.log(bicicleta);
-
+    text[0].innerHTML = `Nome: ${bicicleta.nome}`
+    text[1].innerHTML = (`peso: ${bicicleta.peso}`)
+    console.log(piuPesante);
+    console.log(bicicleta);
+})
+console.log(text[1]);
+console.log(text[2]);
 
