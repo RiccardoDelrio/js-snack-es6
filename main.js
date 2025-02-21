dcf75696f3eb4b692b760949aa63b6401a751a86
RiccardoDelrio <riccardodelrio92@gmail.com>
2025-02-21 15:44:12 +0100

    create an array of object

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log("hi");
+/* Snack 1
+Creare un array di oggetti: Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore. */
+const negozio = [
+    {
+        nome: "terminator",
+        peso: 9,
+    },
+    {
+        nome: "lilly",
+        peso: 15,
+    },
+    {
+        nome: "grazia",
+        peso: 10,
+    },
+    {
+        nome: "graziella",
+        peso: 12,
+    },
+    {
+        nome: "grazieal",
+        peso: 11,
+    }
+
+]
